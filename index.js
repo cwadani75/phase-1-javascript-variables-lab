880dc3f4322f2e7ec6702bda3cb46787db777ab8
cwadani75 <cwadani75@gmail.com>
2025-02-26 20:21:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+const companyName = "Scuber";  // Should be a constant
+
+let mostProfitableNeighborhood = "Chelsea";  // Should be defined with let
+
+let companyCeo = "Susan Smith";  // Should be defined with let
+
